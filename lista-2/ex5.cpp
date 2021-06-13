a4e9f6ad0d1f34ab2e162e6355949f2515178687
Milena Monteiro <42098905+milenamonteiro@users.noreply.github.com>
2021-06-12 23:23:03 -0300

    Create ex5.cpp

---
+++ lista-2/ex5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int matriz[5][5], valor, soma;
+    for (int i = 0; i < 5; i++)
+    for (int j = 0; j < 5; j++) {
+        valor = (rand() % 10) + 1;
+        matriz[i][j] = valor;
+        std::cout << matriz[i][j] << " ";
+        if (j == 4) std::cout << "\n";
+        if (i == j) soma += valor;
+    }
+    std::cout << "Soma dos números da diagonal principal: " << soma;
+}
