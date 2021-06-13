8be0e5712b39ef3912ba66694353dd4ff824730c
Milena Monteiro <42098905+milenamonteiro@users.noreply.github.com>
2021-06-12 22:28:18 -0300

    Update ex4.cpp

--- lista-1/ex4.cpp
+++ lista-1/ex4.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     int ano;
+    std::cout << "Digite o ano de nascimento: ";
     std::cin >> ano;
     if (2021-ano >= 16) std::cout << "Pode votar.";
     else std::cout << "Não pode votar.";
