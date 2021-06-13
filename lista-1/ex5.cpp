444e3415b69b4314ecb157522309e54eb521b085
Milena Monteiro <42098905+milenamonteiro@users.noreply.github.com>
2021-06-12 22:27:42 -0300

    Update ex5.cpp

--- lista-1/ex5.cpp
+++ lista-1/ex5.cpp
@@ -3,21 +3,22 @@
 int main()
 {
     double peso, altura;
+    std::cout << "Digite o peso e a altura para calcular o IMC: ";
     std::cin >> peso >> altura;
-    double imc = peso/altura * altura;
+    double imc = peso/(altura * altura);
 
     if (imc < 20)
-        std::cout << "Abaixo do normal";
+        std::cout << "IMC: " << imc << "\nGrau: Abaixo do normal";
     else if (imc >= 20 && imc < 25)
-        std::cout << "Normal";
+        std::cout << "IMC: " << imc << "\nGrau: Normal";
     else if (imc >= 25 && imc < 30)
-        std::cout << "Sobrepeso";
+        std::cout << "IMC: " << imc << "\nGrau: Sobrepeso";
     else if (imc >= 30 && imc < 35)
-        std::cout << "Obesidade leve";
+        std::cout << "IMC: " << imc << "\nGrau: Obesidade leve";
     else if (imc >= 35 && imc < 40)
-        std::cout << "Obesidade moderada";
+        std::cout << "IMC: " << imc << "\nGrau: Obesidade moderada";
     else
-        std::cout << "Obesidade mórbida";
+        std::cout << "IMC: " << imc << "\nGrau: Obesidade mórbida";
 
     return 0;
 }
