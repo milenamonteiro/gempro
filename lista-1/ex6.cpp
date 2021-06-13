3672340bb14c4b4b5f865fc6e56b9d7ff9270aaa
Milena Monteiro <42098905+milenamonteiro@users.noreply.github.com>
2021-06-12 22:24:03 -0300

    Update ex6.cpp

--- lista-1/ex6.cpp
+++ lista-1/ex6.cpp
@@ -3,13 +3,14 @@
 int main()
 {
     double a, b, c, me;
+    std::cout << "Digite as 3 notas e a média dos exercícios: ";
     std::cin >> a >> b >> c >> me;
     double ma = (a * 1 + b * 2 + c * 3 + me)/7;
 
-    if (ma >= 9) std::cout << "A";
-    else if (ma >= 7.5 && ma < 9.0) std::cout << "B";
-    else if (ma >= 6.0 && ma < 7.5) std::cout << "C";
-    else std::cout << "D";
+    if (ma >= 9) std::cout << "Conceito recebido: A";
+    else if (ma >= 7.5 && ma < 9.0) std::cout << "Conceito recebido: B";
+    else if (ma >= 6.0 && ma < 7.5) std::cout << "Conceito recebido: C";
+    else std::cout << "Conceito recebido: D";
 
     return 0;
 }
