969d18e2c315d4fa19a7b6d1265947fda47ee2eb
Milena Monteiro <42098905+milenamonteiro@users.noreply.github.com>
2021-06-12 23:19:53 -0300

    Update ex4.cpp

--- lista-2/ex4.cpp
+++ lista-2/ex4.cpp
@@ -2,13 +2,13 @@
 
 int main()
 {
-    int matriz[3][3], count = 1;
+    int matriz[3][3], count = 0;
     for (int i = 0; i < 3; i++)
     for (int j = 0; j < 3; j++) {
+        count++; // vai de 1 a 9
         matriz[i][j] = count;
         std::cout << matriz[i][j] << " ";
         if (j == 2) std::cout << "\n"; // quando chega no fim da linha
-        count++; // vai de 1 a 9
     }
 
     return 0;
