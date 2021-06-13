2fa824a7474648682269fddd6818beb4f6792e63
Milena Monteiro <42098905+milenamonteiro@users.noreply.github.com>
2021-06-12 22:56:28 -0300

    Create ex3.cpp

---
+++ lista-2/ex3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int num;
+    std::cout << "Digite um número para ser calculado o fatorial: ";
+    std::cin >> num;
+
+    int fatorial = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fatorial *= i;
+    }
+    std::cout << num << "! = " << fatorial;
+
+    return 0;
+}
